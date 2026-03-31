93360e3d40ece51652593189e68350c3c4f76e68
Rumi007-tech <rudranshmishra659@gmail.com>
2026-03-31 22:46:43 +0530

    Add script to show basic system details
    This script displays basic system details including user, kernel version, uptime, current date and time, and OS name.

--- script1.sh
+++ script1.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
 
+# script1 ---
+# showing some basic system details (nothing fancy)---
+
+echo "System Identity Report"
+echo "----------------------"
+
+# current user :-
+u=$(whoami)
+echo "User -> $u"
+
+# kernel info :
+k=$(uname -r)
+echo "Kernel -> $k"
+
+# uptime (how long system is running) :
+upt=$(uptime -p)
+echo "Uptime -> $upt"
+
+# date and time :-
+d=$(date)
+echo "Time -> $d"
+
+# distro name (taking from os-release)
+# not sure if best way but works
+dist=$(grep PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d '"')
+echo "OS -> $dist"
+
+echo "----------------------"
+echo "done"
